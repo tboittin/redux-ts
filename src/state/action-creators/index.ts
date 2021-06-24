bfd607e46043fefddf476b7a4c6ff43278d037df
Thomas Boittin <tboittin@gmail.com>
2021-06-24 17:31:33 +0200

    33. Applying Typings to Dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,17 +1,30 @@
 import axios from "axios";
+import { Dispatch } from 'redux'
 import { ActionType } from "../action-types";
 import {Action} from '../actions'
 
-const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     })
 
     try {
-      // dispatch({
-      //   type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
-      // })
+      const {data} = await axios.get('https://registry.npmjs.org/-/v1/search',
+      {
+        params: {
+          text: term
+        }
+      })
+
+      const names = data.objects.map((result: any) => (
+        result.package.name
+      ))
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names
+      })
     } catch (err) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
